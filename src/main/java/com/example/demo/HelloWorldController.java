aeee1391844f7b419cad8cfdd2b5f6ec59411d73
Matheus P. Buffon <matheuspbuffon@gmail.com>
2020-03-02 23:26:58 -0300

    Primeiro commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("api/hello-world")
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public List<String> getHelloWorld() {
+        return List.of("Hello", "World");
+    }
+}
